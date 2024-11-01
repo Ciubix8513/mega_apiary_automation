e15161a66071cab519ee419b2e870d76a389f884
Luna <ciubix8514@gmail.com>
2024-11-01 23:48:51 +0400

    Finish? server

--- server.lua
+++ server.lua
@@ -1,9 +1,9 @@
 local sides = require("sides")
 local component = require("component")
 local computer = require("computer")
+
 local inputs_1 = component.list("redstone")
 local inputs = {}
-
 -- Helper for finding redstone IO
 print("Redstone io list:")
 local j = 1
@@ -26,19 +26,45 @@ local input_side = sides.north
 -- Port for communicating with the robot
 local port = 1234
 
+local restocking_index = 0
+
+local function start_restock(index)
+  print(string.format("Out of %s", item_list[index][1]))
+  computer.beep()
+  component.modem.broadcast(port, item_list[index][1])
+  restocking_index = index
+end
+
 -- Main loop
 while true do
   -- Iterate over all the redstone IOs
   for _, i in pairs(inputs) do
     redstone = component.proxy(i)
 
-    if redstone.getInput(input_side) < 5 then
-      print(string.format("Out of %s", item_list[i][1]))
-      computer.beep()
-
-      component.modem.broadcast(port, item_list[i][1])
+    -- if restocking, don't check the item
+    if i == restocking_index then
+      -- If the item has been restocked enough, stop restocking and go into checking mode
+      if redstone.getInput(input_side) >= item_list[i][4] then
+        restocking_index = 0
+      else
+        -- I hate how lua doesn't have fucking continue
+        goto continue
+      end
     end
+
+
+    if redstone.getInput(input_side) < item_list[i][3] then
+      -- If nothing is being restocked, start restocking
+      if restocking_index == 0 then
+        start_restock(i)
+        -- check if the priority of the item is higher
+      elseif item_list[i][2] > item_list[restocking_index][2] then
+        start_restock(i)
+      end
+    end
+    ::continue::
   end
+
   --Check again in a second
   os.sleep(1)
 end
