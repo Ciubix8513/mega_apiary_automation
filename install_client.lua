7e2c748d53d863f94c8816e79754a1b9d1a7fb44
Luna <ciubix8514@gmail.com>
2024-12-27 00:53:36 +0400

    update install scripts

--- install_client.lua
+++ install_client.lua
@@ -1,7 +1,8 @@
 local shell = require("shell")
 
 local repo = "https://raw.githubusercontent.com/ciubix8513/mega_apiary_automation/"
+local branch = "master"
 
-shell.execute(string.format("wget $s/networking.lua", repo))
-shell.execute(string.format("wget $s/client.lua", repo))
-shell.execute(string.format("wget $s/navigation.lua", repo))
+shell.execute(string.format("wget %s%s/networking.lua", branch, repo))
+shell.execute(string.format("wget %s%s/client.lua", branch, repo))
+shell.execute(string.format("wget %s%s/navigation.lua", branch, repo))
--- install_server.lua
+++ install_server.lua
@@ -1,7 +1,8 @@
 local shell = require("shell")
 
 local repo = "https://raw.githubusercontent.com/ciubix8513/mega_apiary_automation/"
+local branch = "master"
 
-shell.execute(string.format("wget $s/networking.lua", repo))
-shell.execute(string.format("wget $s/server.lua", repo))
-shell.execute(string.format("wget $s/item_list.lua", repo))
+shell.execute(string.format("wget %s%s/networking.lua", branch, repo))
+shell.execute(string.format("wget %s%s/server.lua", branch, repo))
+shell.execute(string.format("wget %s%s/item_list.lua", branch, repo))
