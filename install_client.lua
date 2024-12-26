52f39ef084c4205a16039ab27ecb988bb2808373
Luna <ciubix8514@gmail.com>
2024-12-27 01:08:04 +0400

    Fix install scripts?

--- install_client.lua
+++ install_client.lua
@@ -3,6 +3,6 @@ local shell = require("shell")
 local repo = "https://raw.githubusercontent.com/ciubix8513/mega_apiary_automation/"
 local branch = "master"
 
-shell.execute(string.format("wget %s%s/networking.lua", branch, repo))
-shell.execute(string.format("wget %s%s/client.lua", branch, repo))
-shell.execute(string.format("wget %s%s/navigation.lua", branch, repo))
+shell.execute(string.format("wget %s%s/networking.lua", repo, branch))
+shell.execute(string.format("wget %s%s/client.lua", repo, branch))
+shell.execute(string.format("wget %s%s/navigation.lua", repo, branch))
--- install_server.lua
+++ install_server.lua
@@ -3,6 +3,6 @@ local shell = require("shell")
 local repo = "https://raw.githubusercontent.com/ciubix8513/mega_apiary_automation/"
 local branch = "master"
 
-shell.execute(string.format("wget %s%s/networking.lua", branch, repo))
-shell.execute(string.format("wget %s%s/server.lua", branch, repo))
-shell.execute(string.format("wget %s%s/item_list.lua", branch, repo))
+shell.execute(string.format("wget %s%s/networking.lua", repo, branch))
+shell.execute(string.format("wget %s%s/server.lua", repo, branch))
+shell.execute(string.format("wget %s%s/item_list.lua", repo, branch))
